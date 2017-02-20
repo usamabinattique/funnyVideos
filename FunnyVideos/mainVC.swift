fce9583da941c65f1084b95c115799906006a49b
Usama bin Attique <usamabinattique@gmail.com>
2017-02-20 12:40:37 +0500

    first commit

---
+++ FunnyVideos/mainVC.swift
@@ -0,0 +1,20 @@
+//
+//  mainVC.swift
+//  FunnyVideos
+//
+//  Created by S3 Technology on 20/02/2017.
+//  Copyright © 2017 S3 Technologies. All rights reserved.
+//
+
+import UIKit
+
+class mainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+      
+    }
+
+
+}
