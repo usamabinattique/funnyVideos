e54cf165b8e279adf58f4e97a71561bb450460af
Usama bin Attique <usamabinattique@gmail.com>
2017-02-20 12:16:57 +0500

    Initial Commit

---
+++ FunnyVideos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunnyVideos
+//
+//  Created by S3 Technology on 20/02/2017.
+//  Copyright © 2017 S3 Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
